b44286c48b9c6a7719a2e7b3c7bca1ebf400a749
hiba <hibzrashed@gmail.com>
2022-05-24 21:12:23 -0500

    commit tree ae5fd6f6b46a6eed823b2eff5bdb19a7f4342dec

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your code here!
+
+const main = document.getElementById('main')
+main.remove();
+
+//document.body.append('h1')
+//const newHeader = document.createElement('h1');
+//console.log(document.createElement('h1').append(h1#vicotry);ele
+//h1.append(newHeader)
+
+//const yourName = n1
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+//let newHeader = document.getElementById('victory');
+//element.append(h1)
+newHeader.textContent ='YOUR-NAME is the champion';
+document.body.append(newHeader);
+
+//h1.textContent =;
+
+
+
+
+
+//document.getElementById('')
+
+
+//const name = n1
+//document.querySelectorAll('h1')[1].textContent = '${n1} is the champion';
+//h1.append('${n} is the champion');
